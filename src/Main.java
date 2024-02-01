b522000b08c839b7ca50f1c4aa3329888911a6f0
coupachu <coupachu101@gmail.com>
2024-02-01 13:54:03 -0500

    yipee

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static String index = "";
+    public static String selection = "";
     public static void main(String[] args) {
         ArrayList<String> taskList = new ArrayList<>();
-        Scanner input = new Scanner(System.in);
-        String userInput = "1";
-        String selection = "hello";
+
+        String userInput = "";
+        String selection = "";
         while (!userInput.equals("0")){
             printOptions();
             userInput = input.nextLine();
@@ -31,23 +34,21 @@ public class Main {
         System.out.println("(1) add task\n(2) remove task\n(3) update task\n(4) list all tasks\n(0) Exit");
     }
     static void addTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
         System.out.println("type a task to add");
-        String selection = input.nextLine();
+        selection = input.nextLine();
         a.add(selection);
     }
     static void removeTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
         System.out.println("type an index to remove");
-        String selection = input.nextLine();
+        selection = input.nextLine();
         a.remove(Integer.parseInt(selection));
     }
     static void updateTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
+
         System.out.println("type an index to update");
-        String index = input.nextLine();
+        index = input.nextLine();
         System.out.println("type the new value");
-        String selection = input.nextLine();
+        selection = input.nextLine();
         a.set(Integer.parseInt(index),selection);
     }
 }
